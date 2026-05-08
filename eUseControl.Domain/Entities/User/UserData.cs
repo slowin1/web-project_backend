07279994e571688ecb81fdf8277fa09f8b7e04fd
Славик Нагорянский <slaviknagoranskij@MacBook-Air-Slavik.local>
2026-05-08 12:57:07 +0300

    добавленна data Annotation eUseContoro/.Domain/Entities/UserData

--- eUseControl.Domain/Entities/User/UserData.cs
+++ eUseControl.Domain/Entities/User/UserData.cs
@@ -1,21 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using eUseControl.Domain.Entities.services;
 
 namespace eUseControl.Domain.Entities.User;
 
 public class UserData
 {
-   
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; } = string.Empty;
         //имя фамилия 
+        [Required]
+        [StringLength(50), MinLength(5)]
         public string FirstName { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50), MinLength(5)]
         public string LastName { get; set; } = string.Empty;
         //ник пользователя
+        [Required]
+        [StringLength(50), MinLength(5)]
         public string UserName { get; set; } = string.Empty;
-        //данные пользователя
+        //данные пользователяч
+        [Required]
+        [StringLength(50), MinLength(5)]
         public string Email { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50), MinLength(5)]
         public string Password { get; set; } = string.Empty;
+        [Required]
+        [StringLength(50), MinLength(5)]
         public string Phone { get; set; }  = string.Empty;
         //время регистрации 
+        [DataType(DataType.DateTime)]
         public DateTime RegisteredOn { get; set; }
         
         public ICollection<ServiceBookingData> ServiceBookingData { get; set; } = new List<ServiceBookingData>();
