a56ea6ba5969da54aa79a6cd3f0b8e1e5886b6f6
Славик Нагорянский <slaviknagoranskij@MacBook-Air-Slavik.local>
2026-05-08 12:31:26 +0300

    добавленны data annotion для eUseControl.Domain/Entities/service

--- eUseControl.Domain/Entities/services/ServiceBookingData.cs
+++ eUseControl.Domain/Entities/services/ServiceBookingData.cs
@@ -1,13 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+
 namespace eUseControl.Domain.Entities.services;
 
 public class ServiceBookingData
 {
+    [Key]
     public string Id { get; set; } = string.Empty;
     //все про запись на услуги имя описание и т. д.
+    [Required]
     public string BookingId { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100, MinimumLength = 2)]
     public string BookingName { get; set; } = string.Empty;
+    
+    [Required]
+    [StringLength(200, MinimumLength = 30)]
     public string BookingDescription { get; set; } = string.Empty;
     //время записи
+    [Required]
+    [DataType(DataType.Date)]
     public DateTime BookingTime { get; set; }
     public DateTime BookingDate { get; set; }
 }
--- eUseControl.Domain/Entities/services/ServiceTimeSlot.cs
+++ eUseControl.Domain/Entities/services/ServiceTimeSlot.cs
@@ -1,14 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace eUseControl.Domain.Entities.services;
 
 public class ServiceTimeSlot
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public string Id { get; set; } = string.Empty;
+    [Required]
+    [DataType(DataType.DateTime)]
     public DateTime StartTime { get; set; }
+    [Required]
+    [DataType(DataType.DateTime)]
     public DateTime EndTime { get; set; }
     public bool IsAvailable { get; set; } =  true;
     
     
     //внешние ключи
+    [Required]
     public string SpecialistId { get; set; } = string.Empty;
     public string SpecialistName { get; set; } = string.Empty;
 
