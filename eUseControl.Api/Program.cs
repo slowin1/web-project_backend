d73c9156b734eb8388a40a843dad66dfc9828ff1
Славик Нагорянский <slaviknagoranskij@MacBook-Air-Slavik.local>
2026-05-13 09:50:20 +0300

    UserService, IUserService, MappingProfile and UserDto

--- eUseControl.Api/Program.cs
+++ eUseControl.Api/Program.cs
@@ -1,11 +1,17 @@
 using EUseControl.DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
+using eUseControl.BussinessLogic;
+using eUseControl.BussinessLogic.Core;
+using eUseControl.BussinessLogic.Interfaces;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+builder.Services.AddScoped<IUserService, UserService>();
+//подключенние к бд 
 builder.Services.AddDbContext<UserContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
---
+++ eUseControl.Domain/DTO/UserDto.cs
@@ -0,0 +1,36 @@
+namespace eUseControl.Domain.DTOs;
+
+public class CreateUserDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
+
+public class UpdateUserDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+}
+
+public class UserResponseDto
+{
+    public string Id { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public DateTime RegisteredOn { get; set; }
+}
+
+public class LoginDto
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
