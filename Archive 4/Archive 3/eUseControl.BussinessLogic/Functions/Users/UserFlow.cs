24fa4ed9a23ada560a4c2ca7ecb06edb5c3c9e8f
apulaz <kirill.railyan67@gmail.com>
2026-05-16 10:15:46 +0300

    Довленно разделение ролей

---
+++ Archive 4/Archive 3/eUseControl.BussinessLogic/Functions/Users/UserFlow.cs
@@ -0,0 +1,22 @@
+using EUseControl.DataAccess.Context;
+using eUseControl.BussinessLogic.Core.Users;
+using eUseControl.Domain.DTOs;
+
+namespace eUseControl.BussinessLogic.Functions.Users;
+
+public interface IUserFlow
+{
+    Task<IEnumerable<UserResponseDto>> GetAllAsync();
+    Task<UserResponseDto?> GetByIdAsync(string id);
+    Task<UserResponseDto> CreateAsync(CreateUserDto dto);
+    Task<UserResponseDto?> UpdateAsync(string id, UpdateUserDto dto);
+    Task<bool> DeleteAsync(string id);
+}
+
+public class UserFlow : UserActions, IUserFlow
+{
+    public UserFlow(UserContext context)
+        : base(context)
+    {
+    }
+}
